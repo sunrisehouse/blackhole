9166995527255c92db12f2745dff8ce9d2c401d5
seohwan417 <timmy417@snu.ac.kr>
2024-11-21 17:30:54 +0900

    timmy

---
+++ src/App.js
@@ -0,0 +1,367 @@
+import { useEffect, useReducer, useState } from 'react';
+import './App.css';
+import { AppBar, Box, Button, Container, Drawer, IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Toolbar, Typography } from '@mui/material';
+// import { CircularBuffer } from './circulate-buffer';
+import { initAccelerometer, initAudio, initGyroscope } from './sensors';
+import { EventDetector } from './event-detector';
+import SensorsIcon from '@mui/icons-material/Sensors';
+import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
+import PlayCircleFilledRoundedIcon from '@mui/icons-material/PlayCircleFilledRounded';
+import PauseCircleFilledRoundedIcon from '@mui/icons-material/PauseCircleFilledRounded';
+import NotStartedRoundedIcon from '@mui/icons-material/NotStartedRounded';
+import { DebuggingView } from './DebuggingView';
+import { addConsoleLog } from './consolelog';
+import { SettingsView } from './SettingsView';
+
+const APP_VERSION = 'v0.0.14';
+
+// const audioBuffer = new CircularBuffer(10000000);
+// const accelBuffer = new CircularBuffer(10000000);
+// const gyroBuffer = new CircularBuffer(10000000);
+
+let audioContext = null;
+let accelerometer = null;
+let gyroscope = null;
+
+const detector = new EventDetector();
+
+let startTime = 0;
+const INITIALIZE_TIME = 7000;
+const EVENT_FECHING_TIME = 500;
+
+function settingViewReducer(state, action) {
+  if (action.type === 'open') return { ...state, isOpen: true };
+  else if (action.type === 'close') return { ...state, isOpen: false };
+  else if (action.type === 'toggle') return { ...state, isOpen: !state.isOpen };
+  throw Error('Unknown action.');
+}
+
+function measurementReducer(state, action) {
+  if (action.type === 'init') return { ...state, isInit: true };
+  else if (action.type === 'start') return { ...state, isStart: true };
+  else if (action.type === 'pause') return { ...state, isStart: false };
+  throw Error('Unknown action.');
+}
+
+function getUnitSign(unit) {
+  if (unit === 'm') return 'm';
+  else if (unit === 'ft') return 'ft';
+  else if (unit === 'mPerSteps') return 'm/steps';
+  else if (unit === 'ftPerSteps') return 'ft/steps';
+  return '';
+}
+
+function getResultValueString(resultVal, settings) {
+  return `${(
+    resultVal * (
+    settings.unit === 'm' ? settings.mPerM
+    : settings.unit === 'ft' ? settings.ftPerM
+    : settings.unit === 'mPerSteps' ? settings.mPerSteps
+    : settings.unit === 'ftPerSteps' ? settings.ftPerSteps
+    : 1)
+  ).toFixed(1)}`;
+}
+
+function App() {
+  const [settingViewState, dispatchSettingView] = useReducer(settingViewReducer, { isOpen: false });
+  const [measurementState, dispatchMeasurement] = useReducer(measurementReducer, { isInit: false, isStart: false });
+  const [fetchDataIntervalId, setFetchDataIntervalId] = useState(null);
+  const [flagChageLogs, setFlagChangeLogs] = useState([]);
+  const [results, setResults] = useState([]);
+  const [settings, setSettings] = useState({
+    alCoff: 4.5741,
+    blCoff: -1.336,
+    clCoff: 0.0,
+    anrCoff: 1.0403,
+    bnrCoff: 1.3827,
+    cnrCoff: -0.1557,
+    userParameter: 2.5,
+    unit: 'm',
+    mPerM: 1,
+    ftPerM: 3.28084,
+    mPerSteps: 0.8,
+    ftPerSteps: 2.3,
+  });
+
+  useEffect(() => {
+    return () => {
+      if (audioContext) audioContext.close();
+      if (accelerometer) accelerometer.stop();
+      if (gyroscope) gyroscope.stop();
+    };
+  }, []);
+
+  const setfetchDataInterval = () => {
+    setFetchDataIntervalId(
+      setInterval(async () => {
+        const logs = detector.getFlagChangeLog();
+        setFlagChangeLogs((prevLogs) => {
+          if (logs.length > prevLogs.length) {
+            return [...logs];
+          } else {
+            return prevLogs;
+          }
+        });
+        const events = detector.getEventDataList();
+        setResults((results) => {
+          if (events.length > results.length) {
+            return events.map((event) => {
+              const timeDelta = (event.ts2Time - event.ts1Time) * 0.001; // sec 로 변환
+              const laserVal = settings.alCoff * (timeDelta ** settings.blCoff) + settings.clCoff;
+              const resultVal =
+                (settings.anrCoff * (timeDelta ** 2) + settings.bnrCoff * timeDelta + settings.cnrCoff)
+                * settings.userParameter;
+              return {
+                laserVal,
+                resultVal,
+                time: event.trTime - startTime,
+                timeDelta,
+              };
+            });
+          } else {
+            return results;
+          }
+        });
+      }, EVENT_FECHING_TIME)
+    );
+  }
+
+  const handleClickStart = () => {
+    async function initAC() {
+      const { audioContext: ac } = await initAudio((data) => {
+        // audioBuffer.add(data);
+        detector.inputSoundData(data);
+      });
+      audioContext = ac;
+    }
+    async function initAccel() {
+      const { accelerometer: accel } = await initAccelerometer((data) => {
+        // accelBuffer.add(data);
+      });
+      accelerometer = accel;
+    }
+    async function initGyro() {
+      const { gyroscope: gyro } = await initGyroscope((data) => {
+        // gyroBuffer.add(data);
+        detector.inputGyroData(data);
+      });
+      gyroscope = gyro;
+    }
+    initAC();
+    initAccel();
+    initGyro();
+    setTimeout(() => {
+      detector.start();
+      setfetchDataInterval();
+      dispatchMeasurement({ type: 'init' });
+      startTime = Date.now();
+    }, INITIALIZE_TIME);
+    dispatchMeasurement({ type: 'start' });
+  };
+
+  const handleClickPause = () => {
+    detector.stop();
+    if (fetchDataIntervalId) clearInterval(fetchDataIntervalId);
+    dispatchMeasurement({ type: 'pause' });
+  };
+
+  const handleClickRestart = () => {
+    detector.start();
+    setfetchDataInterval();
+    dispatchMeasurement({ type: 'start' });
+  };
+
+  const handleClickApply = ({
+    alCoff, blCoff, clCoff, anrCoff, bnrCoff, cnrCoff, userParameter,
+    unit, mPerM, ftPerM, mPerSteps, ftPerSteps,
+  }) => {
+    try {
+      setSettings({
+        alCoff: Number(alCoff),
+        blCoff: Number(blCoff),
+        clCoff: Number(clCoff),
+        anrCoff: Number(anrCoff),
+        bnrCoff: Number(bnrCoff),
+        cnrCoff: Number(cnrCoff),
+        userParameter: Number(userParameter),
+        unit,
+        mPerM: Number(mPerM),
+        ftPerM: Number(ftPerM),
+        mPerSteps: Number(mPerSteps),
+        ftPerSteps: Number(ftPerSteps),
+      });
+      dispatchSettingView({ type: 'close' })
+    } catch(e) {
+      addConsoleLog(e.message);
+    }
+  };
+
+  return (
+    <div className="App">
+      <AppBar position="static" color="primary">
+        <Toolbar>
+          <SensorsIcon
+            sx={{ mr: 1 }}
+          />
+          <Typography
+            variant="h6"
+            noWrap
+            sx={{
+              mr: 2,
+              display: { md: 'flex' },
+              color: 'inherit',
+              textDecoration: 'none',
+              flexGrow: 1,
+              textAlign: 'left'
+            }}
+          >
+            Blackhole {APP_VERSION}
+          </Typography>
+          <IconButton
+            color="inherit"
+            onClick={() => dispatchSettingView({ type: 'toggle' })}
+          >
+            <SettingsRoundedIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Container maxWidth="sm"
+        sx={{
+          paddingTop: "40px",
+          paddingBottom: "40px",
+        }}
+      >
+        <Paper
+          sx={{ padding: '20px' }}
+        >
+          {measurementState.isInit
+            ? measurementState.isStart
+              ? <Button
+                variant="contained"
+                onClick={handleClickPause}
+                color="secondary"
+              >
+                <PauseCircleFilledRoundedIcon />
+                pause
+              </Button>
+              : <Button
+                variant="contained"
+                onClick={handleClickRestart}
+              >
+                <NotStartedRoundedIcon />
+                restart
+              </Button>
+            : measurementState.isStart
+              ? <Button
+                disabled
+                variant="contained"
+              >
+                initializing
+              </Button>
+              : <Button
+                variant="contained"
+                onClick={handleClickStart}
+              >
+                <PlayCircleFilledRoundedIcon />
+                start
+              </Button>
+          }
+          <Typography
+            variant="h1"
+            sx={{
+              margin: '20px 0',
+              fontSize: '8rem'
+            }}
+          >
+            {!measurementState.isInit && measurementState.isStart
+            ? '##.#'  // 초기화 중일 때만 "##.#" 표시
+            : getResultValueString(
+            results.length > 0
+            ? results[results.length - 1].resultVal
+            : 0,
+            settings
+            )} 
+            <span style={{fontSize:'20px'}}>{getUnitSign(settings.unit)}</span>
+          </Typography>
+          <TableContainer
+            component={Paper}
+            sx={{ marginTop: '20px' }}
+          >
+            <Table>
+              <TableHead>
+                <TableRow>
+                  {/* <TableCell>Parameters</TableCell> */}
+                  <TableCell align="center">ALcoff</TableCell>
+                  <TableCell align="center">BLcoff</TableCell>
+                  <TableCell align="center">CLcoff</TableCell>
+                  <TableCell align="center">ANRcoff</TableCell>
+                  <TableCell align="center">BNRcoff</TableCell>
+                  <TableCell align="center">CNRcoff</TableCell>
+                  <TableCell align="center">User_Parameter</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow>
+                  {/* <TableCell align="center"></TableCell> */}
+                  <TableCell align="center">{settings.alCoff}</TableCell>
+                  <TableCell align="center">{settings.blCoff}</TableCell>
+                  <TableCell align="center">{settings.clCoff}</TableCell>
+                  <TableCell align="center">{settings.anrCoff}</TableCell>
+                  <TableCell align="center">{settings.bnrCoff}</TableCell>
+                  <TableCell align="center">{settings.cnrCoff}</TableCell>
+                  <TableCell align="center">{settings.userParameter}</TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Paper>
+        <TableContainer
+          component={Paper}
+          sx={{ marginTop: '20px' }}
+        >
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell align="center">Time</TableCell>
+                <TableCell align="center">Time_Delta</TableCell>
+                <TableCell align="center">Laser_Val</TableCell>
+                <TableCell align="center">Result_Val</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {results.map((result, index) => (
+                <TableRow
+                  key={index}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell align="center">{result.time}</TableCell>
+                  <TableCell align="center">{result.timeDelta}</TableCell>
+                  <TableCell align="center">{result.laserVal}</TableCell>
+                  <TableCell align="center">{getResultValueString(result.resultVal, settings)}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <Box sx={{ marginTop: '20px' }}>
+          <DebuggingView
+            flagChageLogs={flagChageLogs}
+            startTime={startTime}
+          />
+        </Box>
+      </Container>
+      <Drawer
+        anchor={"left"}
+        open={settingViewState.isOpen}
+        onClose={() => dispatchSettingView({ type: 'toggle' })}
+      >
+        <SettingsView
+          initialSettings={settings}
+          onClickApply={handleClickApply}
+        />
+      </Drawer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/event-detector.js
@@ -0,0 +1,173 @@
+import { addConsoleLog } from "./consolelog";
+
+const TR_WAITING = 7000;
+const TS_CONDITION_MIN_VALUE = 0.4;
+const TS2_CONDITION_MIN_TIME = 230;
+const TS2_CONDITION_MAX_TIME = 2000;
+const TR_CONDITION_MAX_TIME = 70;
+const TR_CONDITION_MIN_VALUE = 0.2;
+
+export class EventDetector {
+  constructor() {
+    this.isOn = false;
+
+    this.eventDataList = []; // 이벤트 데이터를 리스트로 저장
+    this.flagChangeLog = []; // 플래그 변경 이력을 저장
+    
+    this.initializeState();
+  }
+
+  initializeState() {
+    this.flagTs1 = false; // 첫 번째 소리 조건 (기존 flagTr1)
+    this.flagTs2 = false; // 두 번째 소리 조건 (기존 flagTr2)
+    this.flagTr = false;
+
+    this.soundTs1Sample = null;
+    this.soundTs1Time = null;
+    this.soundTs2Sample = null;
+    this.soundTs2Time = null; // flagTs2가 트리거된 시간을 기록
+    this.trEvent = {
+      time: null,
+      value: null,
+    };
+  }
+
+  stop() {
+    this.isOn = false;
+  }
+
+  start() {
+    this.initializeState();
+    this.isOn = true;
+  }
+
+  // 플래그 변경 이력을 기록하는 메소드
+  logFlagChange(flagName, value, t, message) {
+    this.flagChangeLog.push({
+      flag: flagName,
+      value: {
+        ts1: { flag: this.flagTs1, time: this.soundTs1Time, sample: this.soundTs1Sample, },
+        ts2: { flag: this.flagTs2, time: this.soundTs2Time, sample: this.soundTs2Sample, },
+        tr: {
+          flag: this.flagTr,
+          time: this.trEvent.time,
+          a: this.trEvent.value,
+        },
+      },
+      time: t,
+      message,
+    });
+    console.log(`Flag ${flagName} changed to ${value} at time ${t}`);
+    addConsoleLog(`Flag ${flagName} changed to ${value} at time ${t}`)
+  }
+
+  // Sound 데이터 입력
+  inputSoundData({ samples, t }) {
+    if (this.isOn) {
+      if (this.flagTr) {
+        const timeDiff = t - this.trEvent.time;
+        if (timeDiff > TR_WAITING) {
+          this.flagTr = false;
+          this.trEvent.time = null;
+          this.trEvent.value = null;
+          this.logFlagChange('flagTr', false, t, '12초뒤에 flagTr false 로 바꿈');
+        }
+      }
+      if (!this.flagTr) {
+        if (!this.flagTs1) {
+          // 0.4 이상의 값이 있는지 확인하여 flagTs1 설정
+          if (samples.some(sample => sample >= TS_CONDITION_MIN_VALUE)) {
+            this.flagTs1 = true;
+            this.soundTs1Sample = samples.find(sample => sample >= TS_CONDITION_MIN_VALUE);
+            this.soundTs1Time = t;
+            this.logFlagChange('flagTs1', true, t, `${TS_CONDITION_MIN_VALUE} 이상의 sample 발견`);
+          }
+        } 
+        // flagTs1이 설정된 후 100ms ~ 2000ms 사이에 flagTs2 조건 확인
+        else if (this.flagTs1 && !this.flagTs2) {
+          const timeDiff = t - this.soundTs1Time;
+    
+          // 2000 milliseconds가 지났으면 flagTs1을 false로 재설정
+          if (timeDiff > TS2_CONDITION_MAX_TIME) {
+            this.resetFlags(t, `Resetting flagTs1 and flagTs2 after 2000ms timeout.`);
+          } 
+          // 100ms ~ 2000ms 사이에 flagTs2 조건 확인
+          else if (timeDiff >= TS2_CONDITION_MIN_TIME && timeDiff <= TS2_CONDITION_MAX_TIME) {
+            if (samples.some(sample => sample >= TS_CONDITION_MIN_VALUE)) {
+              this.flagTs2 = true;
+              this.soundTs2Sample = samples.find(sample => sample >= TS_CONDITION_MIN_VALUE);
+              this.soundTs2Time = t; // flagTs2가 트리거된 시간 기록
+              this.logFlagChange('flagTs2', true, t, `flagTs1=true 일 때 100 ~ 2000 사이에 ${TS_CONDITION_MIN_VALUE} 이상의 sample 발견`);
+            }
+          }
+        }
+      }
+    }
+  }
+
+  // 가속도 데이터 입력
+  inputGyroData({ a, t }) {
+    if (this.isOn) {
+      if (this.flagTr) {
+        const timeDiff = t - this.trEvent.time;
+        if (timeDiff > TR_WAITING) {
+          this.flagTr = false;
+          this.trEvent.time = null;
+          this.trEvent.value = null;
+          this.logFlagChange('flagTr', false, t, '12초뒤에 flagTr false 로 바꿈');
+        }
+      }
+      if (!this.flagTr) {
+        if (this.flagTs2) {
+          const timeSinceFlagTs2 = t - this.soundTs2Time; // flagTs2가 트리거된 시간과 비교
+    
+          // flagTs2가 설정된 후 70ms 이내에 가속도 값이 0.2 이상이어야 함
+          if (timeSinceFlagTs2 <= TR_CONDITION_MAX_TIME) {
+            if (a >= TR_CONDITION_MIN_VALUE) {
+              this.flagTr = true;
+              this.trEvent.time = t;
+              this.trEvent.value = a;
+              const eventData = {
+                ts1Sample: this.soundTs1Sample,
+                ts1Time: this.soundTs1Time,
+                ts2Sample: this.soundTs2Sample,
+                ts2Time: this.soundTs2Time,
+                trAccel: a,
+                trTime: t,
+              };
+              // 이벤트 데이터를 리스트에 저장
+              this.eventDataList.push(eventData);
+              this.logFlagChange('flagTr', a, t, `Event detected at time ${t}, accel: ${a}`);
+            }
+          } else {
+            // 70ms가 경과하면 flagTs2를 false로 설정
+            this.flagTs2 = false;
+            this.logFlagChange('flagTs2', false, t, `Resetting flagTs2 after 70ms timeout with no sufficient acceleration.`);
+          }
+        }
+      }
+    }
+  }
+
+  // 플래그 초기화
+  resetFlags(t, message) {
+    this.flagTs1 = false;
+    this.flagTs2 = false;
+    this.soundTs1Sample = null;
+    this.soundTs1Time = null;
+    this.soundTs2Sample = null;
+    this.soundTs2Time = null;
+    this.logFlagChange('flagTs1', false, t, message);
+    this.logFlagChange('flagTs2', false, t, message);
+  }
+
+  // 이벤트 데이터를 반환하는 메소드
+  getEventDataList() {
+    return this.eventDataList;
+  }
+
+  // 플래그 변경 이력을 반환하는 메소드
+  getFlagChangeLog() {
+    return this.flagChangeLog;
+  }
+}
